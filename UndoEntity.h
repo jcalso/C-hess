30b398382e1a2016cb45caaefafd8edf4da28e17
Jonathan Calso <jcalso@umich.edu>
2018-07-22 12:47:04 -0400

    Create UndoEntity.h

---
+++ UndoEntity.h
@@ -0,0 +1,34 @@
+#ifndef UNDOENTITY_H
+#define UNDOENTITY_H
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <functional>
+#include <stack>
+
+
+using namespace std;
+
+class UndoEntity {
+public:
+	int prev_row, prev_col;
+	int curr_row, curr_col;
+	string prev_char;
+
+
+	void set_values(int prev_row_in, int prev_col_in, int curr_row_in, int curr_col_in, string prev_char_in) {
+		prev_row = prev_row_in;
+		prev_col = prev_col_in;   //where the piece came from
+
+		prev_char = prev_char_in; //character that was deleted due to piece movement
+
+		curr_col = curr_col_in;
+		curr_row = curr_row_in;
+	}
+};
+
+#endif
